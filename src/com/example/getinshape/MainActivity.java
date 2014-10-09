cab6eeae666c7dc7fb08b4fbfb42d7d3a69c5b93
Alban Delcourt <alban.delcourt@gmail.com>
2014-10-09 14:03:07 +0200

    add Calendar access

--- src/com/example/getinshape/MainActivity.java
+++ src/com/example/getinshape/MainActivity.java
@@ -1,17 +1,49 @@
 package com.example.getinshape;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.CalendarContract.Calendars;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+	// Projection array. Creating indices for this array instead of doing
+	// dynamic lookups improves performance.
+	public static final String[] EVENT_PROJECTION = new String[] {
+	    Calendars._ID,                           // 0
+	    Calendars.ACCOUNT_NAME,                  // 1
+	    Calendars.CALENDAR_DISPLAY_NAME,         // 2
+	    Calendars.OWNER_ACCOUNT                  // 3
+	};
+	  
+	// The indices for the projection array above.
+	private static final int PROJECTION_ID_INDEX = 0;
+	private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+	private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+	private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+	
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH) @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+     // Run query
+        Cursor cur = null;
+        ContentResolver cr = getContentResolver();
+        Uri uri = Calendars.CONTENT_URI;   
+        String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND (" 
+                                + Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                                + Calendars.OWNER_ACCOUNT + " = ?))";
+        String[] selectionArgs = new String[] {"sampleuser@gmail.com", "com.google",
+                "sampleuser@gmail.com"}; 
+        // Submit the query and get a Cursor object back. 
+        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
     }
 
 
